090989d7090ff4b06c5af3369e0b20ac58f1f28a
Hugo <alejaman04@gmail.com>
2023-11-06 15:58:23 -0500

    added option to "drop all"

--- src/DropCommand.java
+++ src/DropCommand.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class DropCommand extends Command {
     private String itemName;
 
@@ -6,13 +9,30 @@ class DropCommand extends Command {
     }
     
     String execute() {
-        try {
-            Item item = GameState.instance().getItemFromInventoryNamed(this.itemName);
-            GameState.instance().getAdventurersCurrentRoom().add(item);
-            GameState.instance().removeFromInventory(item);
-            return itemName + " dropped. \n";
-        } catch(Item.NoItemException nie) {
-            return "Item not found in inventory. \n";
+        if(this.itemName.equals("all")) {
+            if(GameState.instance().getInventory().size() > 0) {
+                List<Item> itemsInInventory = new ArrayList<>(GameState.instance().getInventory());
+                for(Item item : itemsInInventory) {
+                    try {
+                        GameState.instance().removeFromInventory(item);
+                        GameState.instance().getAdventurersCurrentRoom().add(item);
+                    } catch(Item.NoItemException nie) {
+                        
+                    }
+                }
+                return "Dropped all.\n";
+            } else {
+                return "Nothing to drop.\n";
+            }
+        } else {
+            try {
+                Item item = GameState.instance().getItemFromInventoryNamed(this.itemName);
+                GameState.instance().getAdventurersCurrentRoom().add(item);
+                GameState.instance().removeFromInventory(item);
+                return itemName + " dropped. \n";
+            } catch(Item.NoItemException nie) {
+                return "Item not found in inventory. \n";
+            }
         }
     }
 }
