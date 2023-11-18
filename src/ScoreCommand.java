5a6edd0ae95055a733f828a20409f4d80c10dd80
Hugo <alejaman04@gmail.com>
2023-11-18 18:15:57 -0500

    removed spacing error in ScoreCommand

--- src/ScoreCommand.java
+++ src/ScoreCommand.java
@@ -4,15 +4,15 @@ class ScoreCommand extends Command {
     String execute() {
         int score = GameState.instance().getScore();
         if(score < 10) {
-            return "You have accumulated " + score + "points. You are a Novice Adventurer.\n";
+            return "You have accumulated " + score + " points. You are a Novice Adventurer.\n";
         } if(score < 25) {
-            return "You have accumulated " + score + "points. You are a Journeyman Adventurer.\n";
+            return "You have accumulated " + score + " points. You are a Journeyman Adventurer.\n";
         } if(score < 50) {
-            return "You have accumulated " + score + "points. You are a Heroic Adventurer.\n";
+            return "You have accumulated " + score + " points. You are a Heroic Adventurer.\n";
         } if(score < 100) {
-            return "You have accumulated " + score + "points. You are a Legendary Adventurer.\n";
+            return "You have accumulated " + score + " points. You are a Legendary Adventurer.\n";
         }
         
-        return "You have accumulated " + score + "points. You are a Mythical Adventurer.\n";
+        return "You have accumulated " + score + " points. You are a Mythical Adventurer.\n";
     }
 }
